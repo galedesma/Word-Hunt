ba492c57e1c9bd1236a308440408a66fdc3edd42
galedesma <gonza.a.ledesma@gmail.com>
2021-08-04 22:20:03 -0300

    Añado estilos a Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,34 +14,35 @@ const Header = ({category, setCategory, word, setWord}) => {
         },
       });
 
-    return (<div className="header">
-                <span className="title">{word ? word : "Word Hunt"}</span>
-                <div className="inputs">
-                    <ThemeProvider theme={darkTheme}>
-                        <TextField 
-                            label="Select a word" 
-                            className="search"
-                            value={word}
-                            onChange={(e) => setWord(e.target.value)}
-                        />
-                        <TextField
-                            className="select"
-                            select
-                            label="Language"
-                            value={category}
-                            onChange={(e) => setCategory(e.target.value)}
-                            helperText="Please select your currency"
-                            >
-                            {categories.map((option)=>(
-                                <MenuItem key={option.label} value={option.label}>
-                                    {option.value}
-                                </MenuItem>
-                            ))
-                            }
-                        </TextField>
-                    </ThemeProvider>
-                </div>
-            </div>)
+    return (
+    <div className="header">
+        <span className="title">{word ? word : "Word Hunt"}</span>
+        <div className="inputs">
+            <ThemeProvider theme={darkTheme}>
+                <TextField 
+                    label="Select a word" 
+                    className="search"
+                    value={word}
+                    onChange={(e) => setWord(e.target.value)}
+                />
+                <TextField
+                    className="select"
+                    select
+                    label="Language"
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                    >
+                    {categories.map((option)=>(
+                        <MenuItem key={option.label} value={option.label}>
+                            {option.value}
+                        </MenuItem>
+                    ))
+                    }
+                </TextField>
+            </ThemeProvider>
+        </div>
+    </div>
+    )
 }
 
 export default Header
