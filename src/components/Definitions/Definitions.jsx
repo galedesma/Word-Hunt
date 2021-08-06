aeaec5431e4c0005093733705190d4becb1fd89e
galedesma <gonza.a.ledesma@gmail.com>
2021-08-05 21:01:12 -0300

    Añado audio (solo en inglés)

--- src/components/Definitions/Definitions.jsx
+++ src/components/Definitions/Definitions.jsx
@@ -5,6 +5,17 @@ const Definitions = ({word, meanings, category}) => {
     return (
         <div className="meanings">
             {
+                meanings[0] && word && category == "en" && (
+                    <audio 
+                        src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio} 
+                        style={{backgroundColor: "white", borderRadius: "10px"}}
+                        controls
+                    >
+                        Your Browser doesn't support an audio element.
+                    </audio>
+                )
+            }
+            {
                 word === "" ? (
                     <span className='subTitle'>Start by typing a word in Search</span>
                 ) : (
